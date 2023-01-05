6ed164291391f62ff15ec12de7a8ab774976fafe
unistart <2604249649@qq.com>
2023-01-05 13:56:24 +0800

    在config.ts中配置editLink及相关配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -23,16 +23,24 @@ export default defineUserConfig({
   },
   theme: defaultTheme({ // 默认主题配置
     logo: '/logo.png',
+    repo: 'UniStart2/vuepress',
+    editLink: true,
+    editLinkPattern: ':repo/edit/:branch/:path',
+    docsRepo: 'https://github.com/UniStart2/vuepress',
+    docsBranch: 'main',
+    docsDir: 'docs',
     locales: {
       '/': {
-        home: '/README.md',
+        home: '/index.md',
         navbar: [],
+        selectLanguageText: 'Languages',
         selectLanguageName: 'English',
+        editLinkText: 'Edit this page',
       },
       '/zh/': {
-        home: '/zh/README.md',
+        home: '/zh/index.md',
         navbar: [
-          { // navbarItem
+          {
             text: '关于',
             link: '/zh/about/about.md'
           },
@@ -75,7 +83,9 @@ export default defineUserConfig({
             ]
           }
         ],
+        selectLanguageText: '选择语言',
         selectLanguageName: '简体中文',
+        editLinkText: '编辑此页',
       },
     },
   }),
